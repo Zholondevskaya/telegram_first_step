894824690ea4e4d70e3ec5c0dc0b4dd6520c34f6
Victoria Zholondevskaya <zholondevskaya@yandex.ru>
2025-03-13 23:35:52 +0300

    add tests for InMemoryHistoryService

---
+++ src/test/java/org/example/InMemoryHistoryServiceTest.java
@@ -0,0 +1,28 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class InMemoryHistoryServiceTest {
+
+    //what_should_when
+
+    @Test
+    void addHistory_shouldNotThrow_whenGoodParams() {
+        InMemoryHistoryService service = new InMemoryHistoryService();
+        assertDoesNotThrow(() -> service.addHistory(123, "Hello"));
+    }
+
+    @Test
+    void getHistory_shouldNotThrow_whenGoodParams() {
+        InMemoryHistoryService service = new InMemoryHistoryService();
+        assertDoesNotThrow(() -> service.getHistory(123));
+    }
+
+    @Test
+    void deleteHistory_shouldNotThrow_whenGoodParams() {
+        InMemoryHistoryService service = new InMemoryHistoryService();
+        assertDoesNotThrow(() -> service.deleteHistory(123));
+    }
+}
